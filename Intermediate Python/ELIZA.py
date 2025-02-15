93b22bce5c4ec3fae88090a037422e5a527decf2
VeronicaMG <33263432+VeronicaLobos@users.noreply.github.com>
2025-02-15 16:35:18 +0100

    Process the input (find keywords)

--- Intermediate Python/ELIZA.py
+++ Intermediate Python/ELIZA.py
@@ -1,4 +1,17 @@
 
+def check_words(target, words):
+    """
+    Receives a list of words, checks for a match with the
+    words from another list.
+    Prints a message indicating if it found a match or not.
+    """
+    for word in words:
+        if word in target:
+            print("Word found!")
+            break
+
+    print("Word not found!")
+
 def main():
     # Prints a welcome message"
     print("Hello, I am Eliza. I'll be your therapist today.")
@@ -7,16 +20,22 @@ def main():
     prompt = "Tell me your problem (or 'q' anytime to quit): "
 
     # Sets a list of words to look for in user input
-    check: list[str] = ["avoid", "worry", "fear"]
+    keywords = set(["avoid", "worry", "fear"])
 
     # Keeps prompting the user...
     while True:
-        problem = input(prompt)
+        user_problem = input(prompt)
 
         # until user inputs "q"
-        if problem.lower() == "q":
-            print("Ok, bye!")
+        if user_problem.lower() == "q":
+            print("See you next time.")
             break
+
+        # Makes a list of words from input
+        words_in_problem = user_problem.split(" ")
+
+        # Calls a word checker
+        check_words(keywords, words_in_problem)
 
 
 if __name__ == "__main__":
