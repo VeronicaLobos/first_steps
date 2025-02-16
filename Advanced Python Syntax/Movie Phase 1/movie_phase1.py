28e7e8f97cec906f08824e5a5db2135bc3e3c364
VeronicaMG <33263432+VeronicaLobos@users.noreply.github.com>
2025-02-16 12:15:22 +0100

    Print menu and check user input implemented

--- Advanced Python Syntax/Movie Phase 1/movie_phase1.py
+++ Advanced Python Syntax/Movie Phase 1/movie_phase1.py
@@ -1,22 +1,66 @@
+def command_dictionary():
+    """
+    Returns a dictionary pairing the menu options
+    with the functions available to the user in
+    the CLI
+    """
+    pass
+
+
+def check_input():
+    """
+    Asks user for input, a number between 0 and 10
+    Else, will continue asking for a valid input
+    Returns an integer
+    """
+    while True:
+        try:
+            print_menu()
+            user_input = int(input("Enter choice (0-10): "))
+            assert 0 <= user_input <= 10
+            return user_input
+            break
+        except (ValueError, AssertionError):
+            print("Invalid choice")
+            continue
+
+
+def print_menu():
+    menu = ("\nMenu:\n"
+            "0. Exit\n"
+            "1. List movies\n"
+            "2. Add movie\n"
+            "3. Delete movie\n"
+            "4. Update movie\n"
+            "5. Stats\n"
+            "6. Random movie\n"
+            "7. Search movie\n"
+            "8. Movies sorted by rating\n"
+            "9. Movies sorted by year\n"
+            "10. Filter movies\n")
+    print(menu)
+
+
+def main():
+    """
+    A command line interface with a command_dispatcher
+    that performs SCRUM operations with a json file
+    """
+    title = "********** My Movies Database **********"
+    print(title)
+
+    while True:
+        user_input = check_input()
+        # command_dispatcher(user_input)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
 """
-
-title = "********** My Movies Database **********"
-
-Menu:
-0. Exit
-1. List movies
-2. Add movie
-3. Delete movie
-4. Update movie
-5. Stats
-6. Random movie
-7. Search movie
-8. Movies sorted by rating
-9. Movies sorted by year
-10. Filter movies
-
-Enter choice (0-10): 1
-
 ### 1
 
 10 movies in total
